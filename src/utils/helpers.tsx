a6000c1a55b3d62b21ec652caaa430fa35086ca9
GABLRM <g.laroumanie@gmail.com>
2024-02-12 16:05:35 +0100

    change return checkWin

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -8,16 +8,13 @@ export const checkColumn = (column: string[]) => {
     return -1;
 };
 
-export const CheckWin = (grid: string[][], player: string) => {
-    let victory = "";
-
+export const CheckWin = (grid: string[][], player: string): boolean => {
     //COLUMN
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (j < 3) {
                 if (grid[i][j] === player && grid[i][j + 1] === player && grid[i][j + 2] === player && grid[i][j + 3] === player) {
-                    victory = '4 pion ' + player + ' sont en colonne';
-                    return victory;
+                    return true;
                 };
             }
         };
@@ -28,8 +25,7 @@ export const CheckWin = (grid: string[][], player: string) => {
         for (let j = 0; j < grid[i].length; j++) {
             if (i < 4) {
                 if (grid[i][j] === player && grid[i + 1][j] === player && grid[i + 2][j] === player && grid[i + 3][j] === player) {
-                    victory = '4 pion ' + player + ' sont en ligne';
-                    return victory;
+                    return true;
                 };
             }
         };
@@ -40,8 +36,7 @@ export const CheckWin = (grid: string[][], player: string) => {
         for (let j = 0; j < grid[i].length - 3; j++) {
             if (i < 4 && j < 3) {
                 if (grid[i][j] === player && grid[i + 1][j + 1] && grid[i + 2][j + 2] === player && grid[i + 3][j + 3]) {
-                    victory = '4 pion ' + player + ' sont en diagonale'
-                    return victory;
+                    return true;
                 }
             };
         };
@@ -52,12 +47,12 @@ export const CheckWin = (grid: string[][], player: string) => {
         for (let j = 0; j < grid.length - 1; j++) {
             if (i < 4 && j <= 5) {
                 if (grid[i][j] === player && grid[i + 1][j - 1] === player && grid[i + 2][j - 2] === player && grid[i + 3][j - 3] === player) {
-                    victory = '4 pion ' + player + ' sont en diagonale'
-                    return victory;
+                    return true;
                 };
             }
         };
     };
+    return false;
 };
 
 export const checkBoardFull = (grid: string[][]): boolean => {
