99af09ac204d2d13e067321ca09b8f54463cd2b7
Lukabdx <luka.garcia@ynov.com>
2024-02-12 10:08:53 +0100

    New grid order

--- src/atoms/GridState.tsx
+++ src/atoms/GridState.tsx
@@ -3,12 +3,12 @@ import { atom } from "recoil";
 export const gridState = atom({
     key: 'gridState', // unique ID (with respect to other atoms/selectors)
     default: [
+        ["", "red", "", "", "", ""], // Première colonne
+        ["yellow", "", "", "", "", ""],
         ["", "", "", "", "", ""],
         ["", "", "", "", "", ""],
         ["", "", "", "", "", ""],
         ["", "", "", "", "", ""],
-        ["", "", "", "", "", ""],
-        ["", "", "", "", "", ""],
-        ["", "", "", "", "", ""]
+        ["", "", "", "", "", ""] // Dernière colonne
     ], // valeur par défaut (alias valeur initials)
 });
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -9,13 +9,13 @@ const Grid = () => {
     return (
         <RecoilRoot>
             <div className="grid">
-                <Column idCol={6} />
-                <Column idCol={5} />
-                <Column idCol={4} />
-                <Column idCol={3} />
-                <Column idCol={2} />
-                <Column idCol={1} />
                 <Column idCol={0} />
+                <Column idCol={1} />
+                <Column idCol={2} />
+                <Column idCol={3} />
+                <Column idCol={4} />
+                <Column idCol={5} />
+                <Column idCol={6} />
             </div>
         </RecoilRoot>
     );
