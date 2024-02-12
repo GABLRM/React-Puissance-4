fa63b242f1a7382e6c7be97f44569327aaf36a7f
GABLRM <g.laroumanie@gmail.com>
2024-02-12 15:34:30 +0100

    fix check win function

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -11,9 +11,9 @@ export const CheckWin = (grid: string[][], player: string) => {
     let victory = "";
 
     //COLUMN
-    for (let i = 0; i < grid.length - 3; i++) {
+    for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
-            if (i < 3 && j < 2) {
+            if (j < 3) {
                 if (grid[i][j] === player && grid[i][j + 1] === player && grid[i][j + 2] === player && grid[i][j + 3] === player) {
                     victory = '4 pion ' + player + ' sont en colonne';
                     return victory;
@@ -24,8 +24,8 @@ export const CheckWin = (grid: string[][], player: string) => {
 
     //ROW
     for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length - 3; j++) {
-            if (i < 3 && j < 2) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (i < 4) {
                 if (grid[i][j] === player && grid[i + 1][j] === player && grid[i + 2][j] === player && grid[i + 3][j] === player) {
                     victory = '4 pion ' + player + ' sont en ligne';
                     return victory;
@@ -37,7 +37,7 @@ export const CheckWin = (grid: string[][], player: string) => {
     //DIAGONAL DOWN TO TOP
     for (let i = 0; i < grid.length - 3; i++) {
         for (let j = 0; j < grid[i].length - 3; j++) {
-            if (i < 3 && j < 2) {
+            if (i < 4 && j < 3) {
                 if (grid[i][j] === player && grid[i + 1][j + 1] && grid[i + 2][j + 2] === player && grid[i + 3][j + 3]) {
                     victory = '4 pion ' + player + ' sont en diagonale'
                     return victory;
@@ -48,19 +48,13 @@ export const CheckWin = (grid: string[][], player: string) => {
 
     //DIAGONAL TOP TO DOWN
     for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid.length - 3; j++) {
-            if (i < 3 && j < 2) {
-                if (grid[i][j] === player && grid[i + 1][j - 1] && grid[i + 2][j - 2] === player && grid[i + 3][j - 3]) {
+        for (let j = 0; j < grid.length - 1; j++) {
+            if (i < 4 && j <= 5) {
+                if (grid[i][j] === player && grid[i + 1][j - 1] === player && grid[i + 2][j - 2] === player && grid[i + 3][j - 3] === player) {
                     victory = '4 pion ' + player + ' sont en diagonale'
                     return victory;
                 };
             }
         };
     };
-
-    return (
-        <div>
-            state : {victory}
-        </div>
-    )
 };
